1d64fa3ed2445f9939b79c363163ce4b18735fcc
Formation <formation@mbp-de-david-2.home>
2016-06-29 14:54:05 +0200

    Changement de fichier

--- isogram/isogram.php
+++ isogram/isogram.php
@@ -1,15 +1,34 @@
 <?php
-
-$test = "isograms";
-
 function isogram($string){
 
-	$input = explode($string);
+	$array = str_split($string);
+	$x = 0;
 
-	var_dump($input);
+	for($i = 0; $i <= count($array) - 1; $i++){
 
+			$input = $array[$i];
+
+		for($n = 0; $n <= count($array) -1; $n++){
+
+			$compare = $array[$n];
+			if($input === $compare){
+
+				$x++;
+
+			if($x >= 2){
+
+				return "is not isogram";
+
+			}
+
+			}
+
+		}
+
+		$x = 0;
+	}
+
+	return 'is isogram';
 
 }
-
-
 ?>
